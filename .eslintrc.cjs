e8ba09b1c0798f32682987e8ac06963297bfab78
serhii hladchenko <gladseo@gmail.com>
2026-05-28 20:50:05 +0300

    edit eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ["plugin:security/recommended"],
     parserOptions: {
-        "ecmaVersion": 2020,
+        "ecmaVersion": 2022,
         "sourceType": "module"
     },
     env: {
